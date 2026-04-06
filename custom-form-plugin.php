8800c741dfcf4fc44ba71b95a9f0a246c3e6490f
Ami98 <dalwadiami@gmail.com>
2026-04-06 22:28:28 +0530

    create table and shortcode for form

--- custom-form-plugin.php
+++ custom-form-plugin.php
@@ -28,3 +28,28 @@ function cfp_create_table()
 }
 
 register_activation_hook(__FILE__, 'cfp_create_table');
+
+
+function cfp_form_shortcode()
+{
+    ob_start();
+?>
+
+    <form id="cfp-form">
+        <input type="text" name="name" placeholder="Name" required>
+        <input type="email" name="email" placeholder="Email" required>
+        <textarea name="message" placeholder="Message"></textarea>
+
+        <input type="hidden" name="action" value="cfp_submit_form">
+        <?php wp_nonce_field('cfp_nonce_action', 'cfp_nonce'); ?>
+
+        <button type="submit">Submit</button>
+    </form>
+
+    <div id="cfp-response"></div>
+
+<?php
+    return ob_get_clean();
+}
+
+add_shortcode('custom_form', 'cfp_form_shortcode');
