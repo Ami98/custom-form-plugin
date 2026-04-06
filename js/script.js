4dc30091170a30b815f3a4697963d6e704c90509
Ami98 <dalwadiami@gmail.com>
2026-04-06 22:38:13 +0530

    add AJAX js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ function cfp_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'cfp_enqueue_scripts');
 
+jQuery(document).ready(function ($) {
+    $('#cfp-form').submit(function (e) {
+        e.preventDefault();
+
+        var formData = $(this).serialize();
+
+        $.post(cfp_ajax_obj.ajax_url, formData, function (response) {
+            $('#cfp-response').html(response);
+        });
+    });
+});
