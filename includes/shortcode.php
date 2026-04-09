3bbc6548b879664f215d34544db1f9ac40f9a54e
Ami Dalwadi <dalwadiami@gmail.com>
2026-04-09 21:21:47 +0530

    Rename shortcode function and update form ID

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -9,19 +9,18 @@
          
  */
 
-function cfp_form_shortcode_restapi()
+function cfp_form_shortcode()
 {
     ob_start();
 ?>
 
-    <form id="cfp-form-new">
+    <form id="cfp-form">
         <input type="text" name="name" placeholder="Name" required>
         <input type="email" name="email" placeholder="Email" required>
         <textarea name="message" placeholder="Message"></textarea>
 
         <input type="hidden" name="action" value="cfp_submit_form">
-        <?php // @phpstan-ignore-next-line
-        wp_nonce_field('wp_rest_action', 'wp_rest'); ?>
+        <?php wp_nonce_field('cfp_nonce_action', 'cfp_nonce'); ?>
 
         <button type="submit">Submit</button>
     </form>
@@ -32,4 +31,4 @@ function cfp_form_shortcode_restapi()
     return ob_get_clean();
 }
 
-add_shortcode('custom-form-restapi', 'cfp_form_shortcode_restapi');
+add_shortcode('custom_form', 'cfp_form_shortcode');
